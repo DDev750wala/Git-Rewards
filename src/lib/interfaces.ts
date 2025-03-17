24d5bee7c42eb8b349ccacd02f7b4c5c9489600b
Dev750wala <sadisatsowaladev1@gmail.com>
2025-03-17 09:10:46 +0530

    Refactor user model and webhook handler to integrate SignUpWebhookClerk interface and adjust fields

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -68,3 +68,111 @@ interface Repository {
 }
 
 
+// ------------------------------------------------------------------------------
+
+export interface SignUpWebhookClerk {
+    data:             Data;
+    event_attributes: EventAttributes;
+    instance_id:      string;
+    object:           string;
+    timestamp:        number;
+    type:             string;
+}
+
+export interface Data {
+    backup_code_enabled:             boolean;
+    banned:                          boolean;
+    create_organization_enabled:     boolean;
+    created_at:                      number;
+    delete_self_enabled:             boolean;
+    email_addresses:                 EmailAddress[];
+    enterprise_accounts:             any[];
+    external_accounts:               ExternalAccount[];
+    external_id:                     null;
+    first_name:                      string;
+    has_image:                       boolean;
+    id:                              string;
+    image_url:                       string;
+    last_active_at:                  number;
+    last_name:                       string;
+    last_sign_in_at:                 null;
+    legal_accepted_at:               null;
+    locked:                          boolean;
+    lockout_expires_in_seconds:      null;
+    mfa_disabled_at:                 null;
+    mfa_enabled_at:                  null;
+    object:                          string;
+    passkeys:                        any[];
+    password_enabled:                boolean;
+    phone_numbers:                   any[];
+    primary_email_address_id:        string;
+    primary_phone_number_id:         null;
+    primary_web3_wallet_id:          null;
+    private_metadata:                Metadata;
+    profile_image_url:               string;
+    public_metadata:                 Metadata;
+    saml_accounts:                   any[];
+    totp_enabled:                    boolean;
+    two_factor_enabled:              boolean;
+    unsafe_metadata:                 Metadata;
+    updated_at:                      number;
+    username:                        string;
+    verification_attempts_remaining: number;
+    web3_wallets:                    any[];
+}
+
+export interface EmailAddress {
+    created_at:             number;
+    email_address:          string;
+    id:                     string;
+    linked_to:              LinkedTo[];
+    matches_sso_connection: boolean;
+    object:                 string;
+    reserved:               boolean;
+    updated_at:             number;
+    verification:           Verification;
+}
+
+export interface LinkedTo {
+    id:   string;
+    type: string;
+}
+
+export interface Verification {
+    attempts:  null;
+    expire_at: number | null;
+    status:    string;
+    strategy:  string;
+}
+
+export interface ExternalAccount {
+    approved_scopes:   string;
+    avatar_url:        string;
+    created_at:        number;
+    email_address:     string;
+    first_name:        string;
+    id:                string;
+    identification_id: string;
+    image_url:         string;
+    label:             null;
+    last_name:         string;
+    object:            string;
+    provider:          string;
+    provider_user_id:  string;
+    public_metadata:   Metadata;
+    updated_at:        number;
+    username:          string;
+    verification:      Verification;
+}
+
+export interface Metadata {
+}
+
+export interface EventAttributes {
+    http_request: HTTPRequest;
+}
+
+export interface HTTPRequest {
+    client_ip:  string;
+    user_agent: string;
+}
