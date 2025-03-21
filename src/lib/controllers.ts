2927402e5566834106d70a97c9e44de62ff5997d
dev750wala <sadisatsowaladev1@gmail.com>
2025-03-21 11:35:33 +0530

    Fix private key handling in getInstallationToken to ensure proper formatting

--- src/lib/controllers.ts
+++ src/lib/controllers.ts
@@ -146,9 +146,8 @@ export async function handleIssueCommentEvent(payload: IssueComment) {
 
 export async function getInstallationToken(installationId: number) {
     const GITHUB_APP_ID = process.env.GITHUB_APP_ID!
-    const GITHUB_PRIVATE_KEY = process.env.GITHUB_PRIVATE_KEY!
+    const GITHUB_PRIVATE_KEY = process.env.GITHUB_PRIVATE_KEY!.replace(/\\n/g, '\n');
 
-    const GITHUB_PRIVATE_KEY_FINAL = `-----BEGIN RSA PRIVATE KEY-----\n${GITHUB_PRIVATE_KEY}\n-----END RSA PRIVATE KEY-----`;
 
     const payload = {
         iat: Math.floor(Date.now() / 1000) - 60, // issued 1 min ago
@@ -156,7 +155,7 @@ export async function getInstallationToken(installationId: number) {
         iss: GITHUB_APP_ID,
     }
 
-    const jwtToken = jwt.sign(payload, GITHUB_PRIVATE_KEY_FINAL, {
+    const jwtToken = jwt.sign(payload, GITHUB_PRIVATE_KEY, {
         algorithm: 'RS256',
     })
 
