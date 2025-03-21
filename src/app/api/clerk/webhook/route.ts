f0302555d1acaa1dc90d0661b6fd88f529e7f7dc
dev750wala <sadisatsowaladev1@gmail.com>
2025-03-21 09:52:34 +0530

    Check for existing user before creating a new one in webhook handler

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -5,26 +5,32 @@ import { SignUpWebhookClerk } from "@/lib/interfaces"
 import { NextResponse } from "next/server"
 
 export const POST = async (req: Request) => {
-    const req1 = await req.json()
-    const data: SignUpWebhookClerk = req1
+    const req1 = await req.json();
+    const data: SignUpWebhookClerk = req1;
     console.log("webhook data", data);
 
-    const emailAddress = data.data.email_addresses[0]?.email_address! as string
-    const firstname = data.data.first_name
-    const lastname = data.data.last_name
-    const githubId = data.data.external_accounts[0]?.username
+    const emailAddress = data.data.email_addresses[0]?.email_address! as string;
+    const firstname = data.data.first_name;
+    const lastname = data.data.last_name;
+    const githubId = data.data.external_accounts[0]?.username;
 
-    const id = data.data.id
-    
-    await db.user.create({
-        data: {
-            id: id,
-            name: firstname + " " + lastname,
-            emailAddress: emailAddress,
-            githubId: githubId!
-        }
-    })
+    const id = data.data.id;
 
-    return NextResponse.redirect("https://github.com/apps/contriflow/installations/new")
+    const existingUser = await db.user.findUnique({
+        where: { emailAddress }
+    });
 
-}
+    if (!existingUser) {
+        await db.user.create({
+            data: {
+                id: id,
+                name: `${firstname} ${lastname}`,
+                emailAddress: emailAddress,
+                githubId: githubId!
+            }
+        });
+    }
+
+    return NextResponse.redirect("https://github.com/apps/contriflow/installations/new");
+};
+
